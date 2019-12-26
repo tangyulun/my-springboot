dd259603a78b68cf78dd63300922901e56807468
tangyulun <17673256238@163.com>
2019-12-26 21:56:44 +0800

    写一个简单的HashMap

--- src/main/java/com/tyl/hashMap/Map.java
+++ src/main/java/com/tyl/hashMap/Map.java
@@ -6,5 +6,51 @@ package com.tyl.hashMap;
  * @param <V>
  */
 public interface Map<K,V> {
+  /**
+   * 向Map中插入值
+   * @param k
+   * @param v
+   * @return
+   */
+  public V put(K k,V v);
+
+  /**
+   * 根据key获取hashMap中的值
+   * @param k
+   * @return
+   */
+  public V get(K k);
+
+  /**
+   * 获得Map中元素的个数
+   * @return
+   */
+  public int size();
+
+  /**
+   * 获取Map中，键值对的对象
+   * @param <K>
+   * @param <V>
+   */
+  interface Entry<K,V>{
+    /**
+     * getKey
+     * @return
+     */
+    K getKey();
+
+    /**
+     * getValue
+     * @return
+     */
+    V getValue();
+
+    /**
+     * setValue
+     * @param v
+     * @return
+     */
+    V setValue(V v);
+  }
 
 }
